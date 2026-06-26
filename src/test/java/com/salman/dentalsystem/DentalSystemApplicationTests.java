d40937ebbedde56d518ca2acc37621a5d49f4e65
salmansalmanov <salmanov2904@gmail.com>
2026-06-26 16:36:43 +0400

    first commit

---
+++ src/test/java/com/salman/dentalsystem/DentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salman.dentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

21f46c31419fb0e2444d5b5fae5750b94c93f2f0
salmansalmanov <salmanov2904@gmail.com>
2026-06-26 16:43:58 +0400

    feat: add base entity

--- src/main/java/com/salman/dentalsystem/Application.java
+++ src/main/java/com/salman/dentalsystem/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DentalSystemApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DentalSystemApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
---
+++ src/main/java/com/salman/dentalsystem/model/entity/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.salman.dentalsystem.model.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Setter
+@Getter
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}

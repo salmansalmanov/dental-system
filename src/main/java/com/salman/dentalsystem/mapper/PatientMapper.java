eb45554c8ca00600d26e30f7b51f96ba427d0c82
salmansalmanov <salmanov2904@gmail.com>
2026-06-26 19:59:12 +0400

    fix: fix pin issue

--- src/main/java/com/salman/dentalsystem/mapper/PatientMapper.java
+++ src/main/java/com/salman/dentalsystem/mapper/PatientMapper.java
@@ -5,12 +5,19 @@ import com.salman.dentalsystem.model.dto.response.PatientDetailedResponse;
 import com.salman.dentalsystem.model.entity.Patient;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface PatientMapper {
 
     @Mapping(target = "role", constant = "PATIENT")
+    @Mapping(target = "pin", source = "pin", qualifiedByName = "toUpper")
     Patient toEntity(PatientCreateRequest request);
 
     PatientDetailedResponse toDetailedResponse(Patient patient);
+
+    @Named("toUpper")
+    default String toUpper(String value) {
+        return value.toUpperCase();
+    }
 }
--- src/main/java/com/salman/dentalsystem/model/dto/request/PatientCreateRequest.java
+++ src/main/java/com/salman/dentalsystem/model/dto/request/PatientCreateRequest.java
@@ -15,18 +15,18 @@ import lombok.Setter;
 public class PatientCreateRequest {
 
     @NotBlank(message = "Ad boş ola bilməz")
-    @Size(min = 1, max = 20, message = "Simvol sayı '0' ilə '20' arasında ola bilər")
+    @Size(min = 3, max = 20, message = "Simvol sayı '3' ilə '20' arasında ola bilər")
     private String name;
 
     @NotBlank(message = "Soyad boş ola bilməz")
-    @Size(min = 1, max = 20, message = "Simvol sayı '0' ilə '20' arasında ola bilər")
+    @Size(min = 3, max = 20, message = "Simvol sayı '3' ilə '20' arasında ola bilər")
     private String surname;
 
     @NotBlank(message = "Ata adı boş ola bilməz")
-    @Size(min = 1, max = 20, message = "Simvol sayı '0' ilə '20' arasında ola bilər")
+    @Size(min = 3, max = 20, message = "Simvol sayı '3' ilə '20' arasında ola bilər")
     private String patronymic;
 
-    @NotBlank(message = "Telefon nömrəsi adı boş ola bilməz")
+    @NotBlank(message = "Telefon nömrəsi boş ola bilməz")
     @Pattern(regexp = "^\\+994\\d{9}$",
             message = "Telefon nömrəsi +994 ilə başlamalı və 13 simvol uzunluğunda olmalıdır")
     private String phoneNumber;

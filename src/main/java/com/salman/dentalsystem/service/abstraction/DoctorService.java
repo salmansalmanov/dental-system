e400f2cf0b5ec34c0c6ee21ea10685d4be18ba4f
salmansalmanov <salmanov2904@gmail.com>
2026-06-27 16:37:16 +0400

    feat: add doctor get by id api

--- src/main/java/com/salman/dentalsystem/service/abstraction/DoctorService.java
+++ src/main/java/com/salman/dentalsystem/service/abstraction/DoctorService.java
@@ -4,6 +4,10 @@ import com.salman.dentalsystem.model.dto.request.DoctorCreateRequest;
 import com.salman.dentalsystem.model.dto.response.DoctorDetailedResponse;
 import com.salman.dentalsystem.result.DataResult;
 
+import java.util.UUID;
+
 public interface DoctorService {
     DataResult<DoctorDetailedResponse> create(DoctorCreateRequest request);
+
+    DataResult<DoctorDetailedResponse> getById(UUID id);
 }
--- src/main/java/com/salman/dentalsystem/service/concrete/DoctorServiceImpl.java
+++ src/main/java/com/salman/dentalsystem/service/concrete/DoctorServiceImpl.java
@@ -1,9 +1,11 @@
 package com.salman.dentalsystem.service.concrete;
 
+import com.salman.dentalsystem.exception.custom.NotFoundException;
 import com.salman.dentalsystem.mapper.DoctorMapper;
 import com.salman.dentalsystem.model.dto.request.DoctorCreateRequest;
 import com.salman.dentalsystem.model.dto.response.DoctorDetailedResponse;
 import com.salman.dentalsystem.model.entity.Doctor;
+import com.salman.dentalsystem.model.enums.ErrorCode;
 import com.salman.dentalsystem.model.enums.UserStatus;
 import com.salman.dentalsystem.repository.DoctorRepository;
 import com.salman.dentalsystem.result.DataResult;
@@ -11,6 +13,8 @@ import com.salman.dentalsystem.result.SuccessDataResult;
 import com.salman.dentalsystem.service.abstraction.DoctorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +29,13 @@ public class DoctorServiceImpl implements DoctorService {
         Doctor savedDoctor = doctorRepository.save(doctor);
         DoctorDetailedResponse response = doctorMapper.toDetailedResponse(savedDoctor);
         return new SuccessDataResult<>(response, "Həkim yaradıldı");
+    }
+
+    @Override
+    public DataResult<DoctorDetailedResponse> getById(UUID id) {
+        Doctor doctor = doctorRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Həkim tapılmadı: " + id, ErrorCode.DOCTOR_NOT_FOUND));
+        DoctorDetailedResponse response = doctorMapper.toDetailedResponse(doctor);
+        return new SuccessDataResult<>(response, "Həkim tapıldı");
     }
 }
